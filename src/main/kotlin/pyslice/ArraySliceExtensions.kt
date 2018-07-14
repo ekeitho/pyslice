88357e5d8705ddb49432403634f92c2ec81480b9
PravinSonawane <june.pravin@gmail.com>
2018-07-13 18:35:10 -0700

    Improved error messages
    Signed-off-by: PravinSonawane <june.pravin@gmail.com>

--- src/main/kotlin/pyslice/ArraySliceExtensions.kt
+++ src/main/kotlin/pyslice/ArraySliceExtensions.kt
@@ -1,7 +1,7 @@
 package pyslice
 
 operator fun <T> Array<T>.get(slicer: String): Array<T> {
-  require(isValid(slicer)) { "Invalid slice string. Found:$slicer" }
+  require(isValid(slicer)) { "Invalid slicer: $slicer" }
   return when (slicer) {
     "", ":", "::" -> this
     else -> throw AssertionError("Operation not handled")
